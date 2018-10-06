29cec3e53b9906672877caf178c8d3ff2f3fa273
Alan Soares <alanrsoars@gmail.com>
2018-10-06 18:42:03 +1300

    update template

---
+++ templates/domain/api/__tests__/api.spec.ts
@@ -0,0 +1,5 @@
+describe("{domain}/api", () => {
+  it("shoudl work", () => {
+    expect(2 + 2).toBe(4);
+  });
+});
---
+++ templates/domain/reducers/__tests__/reducers.spec.ts
@@ -0,0 +1,5 @@
+describe("{domain}/reducers", () => {
+  it("shoudl work", () => {
+    expect(2 + 2).toBe(4);
+  });
+});
