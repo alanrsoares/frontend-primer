d98f01fbf91c7c5896bd681db1588ae64060b714
Alan Soares <alanrsoars@gmail.com>
2018-10-06 18:37:49 +1300

    fix import

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import './index.scss';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
