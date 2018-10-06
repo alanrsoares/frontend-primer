7ff97604a9389cbc95561961ab9ff3cbe194f4c2
Alan Soares <alanrsoars@gmail.com>
2018-10-06 19:43:51 +1300

    add module alias

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+
 import UI from "./ui";
 
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<UI />, document.getElementById("root") as HTMLElement);
+
 registerServiceWorker();
