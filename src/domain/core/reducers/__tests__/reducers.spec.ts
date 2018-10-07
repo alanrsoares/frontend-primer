fc49dc6c72feb2df24a33e7347dc8a6d8bb73262
Alan Soares <alanrsoars@gmail.com>
2018-10-07 22:22:33 +1300

    add dummy tests for core sagas+reducers

---
+++ src/domain/core/reducers/__tests__/reducers.spec.ts
@@ -0,0 +1,5 @@
+describe("Core.reducers", () => {
+  it("should work", () => {
+    expect(2 + 2).toBe(4);
+  });
+});
---
+++ src/domain/core/sagas/__tests__/sagas.spec.ts
@@ -0,0 +1,5 @@
+describe("Core.sagas", () => {
+  it("should work", () => {
+    expect(2 + 2).toBe(4);
+  });
+});
