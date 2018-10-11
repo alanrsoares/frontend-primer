af97da05037a4420e1854b258487bd83de1d64d9
Alan Soares <alanrsoars@gmail.com>
2018-10-11 14:09:13 +1300

    add logger middleware

--- src/domain/core/redux/index.ts
+++ src/domain/core/redux/index.ts
@@ -3,21 +3,27 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga";
 
 import saga from "@domain/core/redux/middleware/saga";
+import logger from "@domain/core/redux/middleware/logger";
 
 import core from "@domain/core/reducers";
 import genres from "@domain/genres/reducers";
 
 export * from "@domain/core/redux/types";
 
-const sagaMiddleware = createSagaMiddleware();
-
+// combines all of the domain reducers into a single reducer function
 const rootReducer = combineReducers({
   core,
   genres
 });
 
+// set-up redux middlewares
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middleware = [sagaMiddleware, logger];
+
 export function configureStore() {
-  const storeEnhancer = composeWithDevTools(applyMiddleware(sagaMiddleware));
+  const storeEnhancer = composeWithDevTools(applyMiddleware(...middleware));
   const store = createStore(rootReducer, storeEnhancer);
 
   sagaMiddleware.run(saga);
