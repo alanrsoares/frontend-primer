cb567286db2ff38b3fb615fc5a9da422cca02786
Alan Soares <alanrsoars@gmail.com>
2018-10-12 18:09:03 +1300

    remove shows

--- src/domain/shows/api/__tests__/api.spec.ts
+++
@@ -1,5 +0,0 @@
-describe("shows/api", () => {
-  it("shoudl work", () => {
-    expect(2 + 2).toBe(4);
-  });
-});
--- src/domain/shows/sagas/__tests__/sagas.spec.ts
+++
@@ -1,5 +0,0 @@
-describe("shows/sagas", () => {
-  it("shoudl work", () => {
-    expect(2 + 2).toBe(4);
-  });
-});
