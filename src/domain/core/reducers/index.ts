3e604a8ad3b54cf64accdd5ac4cf5b29421b7c1c
Alan Soares <alanrsoars@gmail.com>
2018-10-26 14:56:38 +1300

    cleaner reducer aggregation

--- src/domain/core/reducers/index.ts
+++ src/domain/core/reducers/index.ts
@@ -3,13 +3,7 @@ import { handleActions } from "re-reduced";
 
 import { indexBy } from "@helpers/list";
 
-import {
-  actions,
-  State,
-  UserState,
-  FeaturesState,
-  Breadcrumb
-} from "@domain/core";
+import { actions, State } from "@domain/core";
 
 const INITIAL_STATE: State = {
   features: {
@@ -24,36 +18,30 @@ const INITIAL_STATE: State = {
   breadcrumbs: []
 };
 
-const features = handleActions<FeaturesState>(
-  actions.features.fetch.success.reduce((_, payload) => ({
-    byId: indexBy("id", payload),
-    idList: payload.map(feature => feature.id)
-  })),
-  INITIAL_STATE.features
-);
-
-const user = handleActions<UserState>(
-  [
-    actions.user.login.request.reduce((state, _) => ({
-      ...state,
-      isLoggingIn: true
-    })),
-    actions.user.login.success.reduce((_, profile) => ({
-      profile,
-      isAuthenticated: true,
-      isLoggingIn: false
-    }))
-  ],
-  INITIAL_STATE.user
-);
-
-const breadcrumbs = handleActions<Breadcrumb[]>(
-  actions.setBreadcrumbs.reduce((_, payload) => payload),
-  INITIAL_STATE.breadcrumbs
-);
-
 export default combineReducers<State>({
-  user,
-  features,
-  breadcrumbs
+  user: handleActions(
+    [
+      actions.user.login.request.reduce((state, _) => ({
+        ...state,
+        isLoggingIn: true
+      })),
+      actions.user.login.success.reduce((_, profile) => ({
+        profile,
+        isAuthenticated: true,
+        isLoggingIn: false
+      }))
+    ],
+    INITIAL_STATE.user
+  ),
+  features: handleActions(
+    actions.features.fetch.success.reduce((_, payload) => ({
+      byId: indexBy("id", payload),
+      idList: payload.map(feature => feature.id)
+    })),
+    INITIAL_STATE.features
+  ),
+  breadcrumbs: handleActions(
+    actions.setBreadcrumbs.reduce((_, payload) => payload),
+    INITIAL_STATE.breadcrumbs
+  )
 });
