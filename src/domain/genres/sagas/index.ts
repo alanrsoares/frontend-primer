378ba4d54afc7277569f8ed137e899fad564e066
Alan Soares <alanrsoars@gmail.com>
2018-11-30 12:33:33 +1300

    rearrange package.json dependencies

--- src/domain/genres/sagas/index.ts
+++ src/domain/genres/sagas/index.ts
@@ -2,7 +2,8 @@ import { SagaIterator } from "redux-saga";
 import { takeLatest } from "redux-saga/effects";
 
 import actions from "@domain/genres/actions";
-import fetchGenres from "@domain/genres/sagas/fetchGenres";
+
+import fetchGenres from "./fetchGenres";
 
 export default function* sagaWatcher(): SagaIterator {
   yield takeLatest(actions.fetchGenres.type, fetchGenres);
