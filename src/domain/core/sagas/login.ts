9bbba9f3c4b20941bca44c8d9b278f5b0fcb532c
Alan Soares <alanrsoars@gmail.com>
2019-02-20 16:46:14 +1300

    these types can be inferred

--- src/domain/core/sagas/login.ts
+++ src/domain/core/sagas/login.ts
@@ -2,11 +2,7 @@ import { apiWorkerFactory } from "re-reduced";
 
 import actions from "@domain/core/actions";
 import * as api from "@domain/core/api";
-import { UserProfile, LoginPayload } from "../types";
 
-const loginWorker = apiWorkerFactory<UserProfile, LoginPayload>(
-  actions.user.login,
-  api.login
-);
+const loginWorker = apiWorkerFactory(actions.user.login, api.login);
 
 export default loginWorker;
