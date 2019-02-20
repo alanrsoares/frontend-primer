a244bcb2711ac9819aa569e3fd5f4f4e7ef36384
Alan Soares <alanrsoars@gmail.com>
2019-02-20 14:23:25 +1300

    update tests

--- src/domain/core/sagas/__tests__/sagas.spec.ts
+++ src/domain/core/sagas/__tests__/sagas.spec.ts
@@ -1,5 +1,27 @@
+import { testSaga } from "redux-saga-test-plan";
+
+import { Feature } from "@domain/core/types";
+
+import actions from "@domain/core/actions";
+import * as api from "@domain/core/api";
+
+import fetchFeatures from "../fetchFeatures";
+
 describe("Core.sagas", () => {
-  it("should work", () => {
-    expect(2 + 2).toBe(4);
+  describe("fetchFeatures", () => {
+    it("should fetch features successfully", () => {
+      const triggerAction = actions.features.fetch;
+      const mockApiResponse: Feature[] = [{ id: "id-1", isEnabled: true }];
+
+      testSaga(fetchFeatures, triggerAction())
+        .next()
+        .put(triggerAction.request())
+        .next()
+        .call(api.fetchFeatures)
+        .next(mockApiResponse)
+        .put(triggerAction.success(mockApiResponse))
+        .next()
+        .isDone();
+    });
   });
 });
