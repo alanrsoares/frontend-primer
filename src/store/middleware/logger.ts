c37eb6fdddfe013b513df19f459c96c524d13693
Alan Soares <alanrsoars@gmail.com>
2019-02-28 17:45:43 +1300

    text change

--- src/store/middleware/logger.ts
+++ src/store/middleware/logger.ts
@@ -4,7 +4,7 @@ import { message } from "antd";
 
 const logger: Middleware = _ => next => (action: Action) => {
   if (action.type.endsWith("FAILURE")) {
-    message.error("Snap! Something went wrong :(");
+    message.error("Oh snap! Something went wrong :(");
 
     console.error(
       `Action "${action.type.replace("_FAILURE", "")}" failed with:`,
