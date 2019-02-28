19332b94944833c4f4e79c6468d2cb9ec8b4872c
Alan Soares <alanrsoars@gmail.com>
2019-02-28 17:51:31 +1300

    minor cleanup

--- src/store/middleware/logger.ts
+++ src/store/middleware/logger.ts
@@ -1,10 +1,11 @@
 import { Middleware } from "redux";
 import { Action } from "re-reduced";
-import { message } from "antd";
+
+import messageService from "antd/lib/message";
 
 const logger: Middleware = _ => next => (action: Action) => {
   if (action.type.endsWith("FAILURE")) {
-    message.error("Oh snap! Something went wrong :(");
+    messageService.error("Oh snap! Something went wrong :(");
 
     console.error(
       `Action "${action.type.replace("_FAILURE", "")}" failed with:`,
