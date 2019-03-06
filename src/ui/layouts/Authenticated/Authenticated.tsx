56bc29e3a023f38631b9df1d1684654fa9811713
Alan Soares <alanrsoars@gmail.com>
2019-03-07 12:39:59 +1300

    prefer functional components

--- src/ui/layouts/Authenticated/Authenticated.tsx
+++ src/ui/layouts/Authenticated/Authenticated.tsx
@@ -29,75 +29,59 @@ interface Props extends RouteComponentProps {
   actions: typeof actions;
 }
 
-class Authenticated extends React.Component<Props> {
-  public render() {
-    return (
-      <Layout tagName="section" className={styles.layout}>
-        <Layout.Header tagName="header" style={{ display: "flex" }}>
-          <div className={styles.logo} />
-          <div style={{ flexGrow: 1 }}>{this.renderMenu()}</div>
-          <div className={styles.user}>
-            <Button onClick={this.handleLogout}>Logout</Button>
-          </div>
-        </Layout.Header>
-        <Layout.Content tagName="section" className={styles.content}>
-          <Breadcrumb className={styles.breadcrumbs}>
-            {this.renderBreadcrumbs()}
-          </Breadcrumb>
-          <div className={styles.page}>{this.renderPage()}</div>
-        </Layout.Content>
-        <Layout.Footer tagName="footer" className={styles.footer}>
-          Frontend Primer ©{new Date().getFullYear()}
-        </Layout.Footer>
-      </Layout>
-    );
-  }
-
-  public renderMenu() {
-    return (
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        className={styles.menu}
-        selectedKeys={[this.props.location.pathname]}
-      >
-        {Object.keys(ROUTES).map(key => {
-          const item = ROUTES[key] as string;
-
-          return (
-            <Menu.Item key={item}>
-              <Link to={item}>{capitalize(key)}</Link>
-            </Menu.Item>
-          );
-        })}
-      </Menu>
-    );
-  }
-
-  public renderBreadcrumbs() {
-    const toItem = (breadcrumb: BreadcrumbItem) => (
-      <Breadcrumb.Item key={breadcrumb.text || "home"}>
-        {breadcrumb.icon && <Icon type={breadcrumb.icon} />}
-        {breadcrumb.text}
-      </Breadcrumb.Item>
-    );
-
-    return this.props.breadcrumbs.map(toItem);
-  }
-
-  public renderPage() {
-    return (
-      <Switch>
-        <Route exact path={ROUTES.home} component={Dashboard} />
-        <Route path={ROUTES.genres} component={Genres} />
-        <Route path={ROUTES.movies} component={Movies} />
-      </Switch>
-    );
-  }
-
-  private handleLogout = () => {
-    this.props.actions.user.logout();
+function Authenticated(props: Props) {
+  const handleLogout = () => {
+    props.actions.user.logout();
   };
+
+  return (
+    <Layout tagName="section" className={styles.layout}>
+      <Layout.Header tagName="header" style={{ display: "flex" }}>
+        <div className={styles.logo} />
+        <div style={{ flexGrow: 1 }}>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            className={styles.menu}
+            selectedKeys={[props.location.pathname]}
+          >
+            {Object.keys(ROUTES).map(key => {
+              const item = ROUTES[key] as string;
+
+              return (
+                <Menu.Item key={item}>
+                  <Link to={item}>{capitalize(key)}</Link>
+                </Menu.Item>
+              );
+            })}
+          </Menu>
+        </div>
+        <div className={styles.user}>
+          <Button onClick={handleLogout}>Logout</Button>
+        </div>
+      </Layout.Header>
+      <Layout.Content tagName="section" className={styles.content}>
+        <Breadcrumb className={styles.breadcrumbs}>
+          {props.breadcrumbs.map(breadcrumb => (
+            <Breadcrumb.Item key={breadcrumb.text || "home"}>
+              {breadcrumb.icon && <Icon type={breadcrumb.icon} />}
+              {breadcrumb.text}
+            </Breadcrumb.Item>
+          ))}
+        </Breadcrumb>
+        <div className={styles.page}>
+          <Switch>
+            <Route exact path={ROUTES.home} component={Dashboard} />
+            <Route path={ROUTES.genres} component={Genres} />
+            <Route path={ROUTES.movies} component={Movies} />
+          </Switch>
+        </div>
+      </Layout.Content>
+      <Layout.Footer tagName="footer" className={styles.footer}>
+        Frontend Primer ©{new Date().getFullYear()}
+      </Layout.Footer>
+    </Layout>
+  );
 }
 
 const enhance = connectWithActions<Props>(actions, {
