389931fcc4a1cb35d0ddca18990924c2346f78d6
Alan Soares <alanrsoars@gmail.com>
2019-03-07 15:37:03 +1300

    add propper splash screen

--- src/ui/components/Splash/Splash.tsx
+++ src/ui/components/Splash/Splash.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import { ReactComponent as Icon } from "./icon.svg";
 import styles from "./Splash.module.css";
 
 export default function Splash() {
-  return <div className={styles.container}>Initializing the app...</div>;
+  return (
+    <div className={styles.container}>
+      <Icon className={styles.spinner} />
+      <div className={styles.text}>Frontend Primer is initializing...</div>
+    </div>
+  );
 }
